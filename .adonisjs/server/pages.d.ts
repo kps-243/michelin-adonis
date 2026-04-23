8745cb43b23039de8272779f35d95f8cba43e404
WilliammCorreia <william.correia@edu.ece.fr>
2026-04-23 15:29:13 +0200

    Ajoute le adonisjs

--- .adonisjs/server/pages.d.ts
+++ .adonisjs/server/pages.d.ts
@@ -18,13 +18,13 @@ declare module '@adonisjs/inertia/types' {
     'errors/server_error': ExtractProps<(typeof import('../../inertia/pages/errors/server_error.tsx'))['default']>
     'home': ExtractProps<(typeof import('../../inertia/pages/home.tsx'))['default']>
     'PourToi': ExtractProps<(typeof import('../../inertia/pages/PourToi.tsx'))['default']>
-    'Swipe': ExtractProps<(typeof import('../../inertia/pages/Swipe.tsx'))['default']>
+    'Profile': ExtractProps<(typeof import('../../inertia/pages/Profile.tsx'))['default']>
+    'profile/edit': ExtractProps<(typeof import('../../inertia/pages/profile/edit.tsx'))['default']>
     'restaurant/edit': ExtractProps<(typeof import('../../inertia/pages/restaurant/edit.tsx'))['default']>
     'restaurant/index': ExtractProps<(typeof import('../../inertia/pages/restaurant/index.tsx'))['default']>
     'restaurant/single': ExtractProps<(typeof import('../../inertia/pages/restaurant/single.tsx'))['default']>
-    'Profile': ExtractProps<(typeof import('../../inertia/pages/Profile.tsx'))['default']>
     'Restaurants': ExtractProps<(typeof import('../../inertia/pages/Restaurants.tsx'))['default']>
     'Sejours': ExtractProps<(typeof import('../../inertia/pages/Sejours.tsx'))['default']>
-    'profile/edit': ExtractProps<(typeof import('../../inertia/pages/profile/edit.tsx'))['default']>
+    'Swipe': ExtractProps<(typeof import('../../inertia/pages/Swipe.tsx'))['default']>
   }
 }
